2b4d3b2017d995527aa4bab47ba0b4315e9f08fd
hoang765218 <mvhoang765218@gmail.com>
2022-08-08 16:39:20 +0700

    buoi1_createComponent_databinding

---
+++ src/Components/BaiTapLayout/BTFooter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BTFooter extends Component {
+  render() {
+    return (
+      <div className="bg-secondary text-white display-4 p-5 text-center ">
+        BTFooter
+      </div>
+    );
+  }
+}
